8e7c0f76e8a148f2a0e1e57c13b900b20701d37d
IFrysiak <165841957+IFrysiak@users.noreply.github.com>
2024-05-07 03:54:29 +0200

    Update PQArrayMax.hpp

--- headers/PQArrayMax.hpp
+++ headers/PQArrayMax.hpp
@@ -1,12 +1,38 @@
 #pragma once
 #include "PriorityQueue.hpp"
 
-class PQArrayMax : public PriorityQueue {
+struct Element {
+    uint32_t value;
+    uint32_t priority;
+};
+
+class ArrayList {
+private:
+    Element* array_;
+    int initialCapacity_;
+    int capacity_;
+    int size_;
+
 public:
-    PQArrayMax() : PriorityQueue() {};
-    PQArrayMax(const PQArrayMax &other); // Konstruktor kopiujący
+    ArrayList(int initialCapacity = 10);
+    ~ArrayList();
+    void addLast(Element element);
+    Element getElement(int index) const;
+    void removeFrom(int index);
+    int getSize() const;
+    
+};
+
+class PQArrayMax : public PriorityQueue {
+private:
+    ArrayList array_;
+    int findMaxIndex() const;
+
+public:
+    PQArrayMax(int capacity = 10);
     void insert(uint32_t element, uint32_t priority) override;
+    void insert(Node node) override;
     Node extractMax() override;
     Node peek() override;
-    void modifyKey(uint32_t element, uint32_t priority) override;
+    void modifyKey(uint32_t element, uint32_t newPriority) override;
 };
