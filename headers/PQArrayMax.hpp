5f9bda960cc8263f845bc0632bd8ac088c76d95f
IFrysiak <165841957+IFrysiak@users.noreply.github.com>
2024-05-08 12:53:49 +0200

    dodano komentarze

--- headers/PQArrayMax.hpp
+++ headers/PQArrayMax.hpp
@@ -4,12 +4,12 @@
 
 class PQArrayMax : public PriorityQueue {
 private:
-    int findMaxIndex() const;
+    int findMaxIndex() const;  //funkcja znajdująca największy indeks
 public:
     PQArrayMax() : PriorityQueue() {};
-    void insert(uint32_t element, uint32_t priority) override;
-    void insert(Node node) override;
-    Node extractMax() override;
-    Node peek() override;
-    void modifyKey(uint32_t element, uint32_t newPriority) override;
+    void insert(uint32_t element, uint32_t priority) override;  //dodawanie elementu o priorytecie na koniec kolejki
+    void insert(Node node) override;  //dodawanie węzła na koniec kolejki
+    Node extractMax() override;  //usuwanie elementu z kolejki o pierwszym najwyższym priorytecie
+    Node peek() override;  //zwrócenie elementu o pierwszym najwyższym priorytecie
+    void modifyKey(uint32_t element, uint32_t newPriority) override;  //zmiana priorytetu pierwszego wystąpinia elementu
 };
